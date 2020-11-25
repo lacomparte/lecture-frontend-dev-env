44767190c83242ff807b672e036414e3b5a52a3c
김상락/UI개발팀 <ksrrr.rak@musinsa.com>
2020-11-25 15:31:10 +0900

    완료

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,22 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext][hash]',
+          publicPath: './dist',
+          limit: 10000,
+        }
+      }
+    ],
   }
 }
